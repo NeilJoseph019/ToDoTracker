e7398210ff01b05b49538f669c9236197e9431e7
Neil Joseph Pulikkathara <neil.joseph@ibm.com>
2022-09-20 18:00:15 +0100

    Done with create and update views using class-based views

--- todo_list/todo/urls.py
+++ todo_list/todo/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.TodoTaskView.as_view(), name= "todoPage")
+    path("", views.TodoTaskView.as_view(), name= "todoPage"),
+    path("<int:pk>", views.TaskInDetail.as_view(), name = "taskInDetail"),
+    path("Enter-New-Task", views.EnterTasknDetails.as_view(), name = "enterTaskDetails"),
+    path("Update-Task/<int:pk>", views.UpdateTask.as_view(), name = "updateTaskDetails")
 ]
--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from . import models
 
 class TodoTaskView (ListView):
@@ -8,4 +11,19 @@ class TodoTaskView (ListView):
     context_object_name = "tasks"
     ordering = ["-timestamp"]
 
+class TaskInDetail(DetailView):
+    template_name = "todo/taskDetail.html"
+    model = models.Task
+    
+class EnterTasknDetails(CreateView):
+    template_name = "todo/enterTaskDetails.html"
+    model = models.Task
+    fields = "__all__"
+    success_url = reverse_lazy('todoPage')
+    
+class UpdateTask(UpdateView):
+    template_name = "todo/UpdateTaskDetails.html"
+    model = models.Task
+    fields = "__all__"
+    success_url = reverse_lazy('todoPage')
 
