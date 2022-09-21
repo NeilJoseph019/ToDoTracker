164e4d32541a77ea0ba8d348330e47389b20df24
Neil Joseph Pulikkathara <neil.joseph@ibm.com>
2022-09-22 00:20:53 +0100

    Tasks displayed according to the logged-in user authentication.

--- todo_list/todo/urls.py
+++ todo_list/todo/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.TodoTaskView.as_view(), name= "todoPage"),
+    path("", views.loginPageView.as_view(), name="loginPage"),
+    path("logout", views.logoutPageView.as_view(), name="logoutPage"),
+    path("To-Do-Tasks", views.TodoTaskView.as_view(), name= "todoPage"),
     path("<int:pk>", views.TaskInDetail.as_view(), name = "taskInDetail"),
     path("Enter-New-Task", views.EnterTasknDetails.as_view(), name = "enterTaskDetails"),
     path("Update-Task/<int:pk>", views.UpdateTask.as_view(), name = "updateTaskDetails"),
--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -2,14 +2,33 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from . import models
 
-class TodoTaskView (ListView):
+
+class loginPageView(LoginView):
+    template_name = "todo/login.html"
+    fields = "__all__"
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy("todoPage")
+
+class logoutPageView(LogoutView):
+    next_page = "loginPage"
+
+class TodoTaskView(ListView):
     template_name = "todo/todo.html"
     model = models.Task
     context_object_name = "tasks"
     ordering = ["timestamp"]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tasks"] = context["tasks"].filter(user=self.request.user)
+        return context
+    
 
 class TaskInDetail(DetailView):
     template_name = "todo/taskDetail.html"
