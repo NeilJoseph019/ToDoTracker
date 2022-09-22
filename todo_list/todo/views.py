96454d2361ca8096814fab6b6c87de032e757d43
Neil Joseph Pulikkathara <neil.joseph@ibm.com>
2022-09-22 11:56:42 +0100

    loginRequiredMixin is added, user specific data is displayed using filter(user= request.user), and def form_valid - form.instance.user = self.request.user was implemented in the create view

--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -3,13 +3,14 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from . import models
 
 
 class loginPageView(LoginView):
     template_name = "todo/login.html"
-    fields = "__all__"
+    fields = "__all__"  # '__all__' is required because it needs to access all the fileds of the user model
     redirect_authenticated_user = True
 
     def get_success_url(self):
@@ -18,7 +19,7 @@ class loginPageView(LoginView):
 class logoutPageView(LogoutView):
     next_page = "loginPage"
 
-class TodoTaskView(ListView):
+class TodoTaskView(LoginRequiredMixin, ListView):
     template_name = "todo/todo.html"
     model = models.Task
     context_object_name = "tasks"
@@ -30,23 +31,28 @@ class TodoTaskView(ListView):
         return context
     
 
-class TaskInDetail(DetailView):
+class TaskInDetail(LoginRequiredMixin, DetailView):
     template_name = "todo/taskDetail.html"
     model = models.Task
     
-class EnterTasknDetails(CreateView):
+class EnterTasknDetails(LoginRequiredMixin, CreateView):
     template_name = "todo/enterTaskDetails.html"
     model = models.Task
-    fields = "__all__"
-    success_url = reverse_lazy('todoPage')
-    
-class UpdateTask(UpdateView):
-    template_name = "todo/updateTaskDetails.html"
-    model = models.Task
-    fields = "__all__"
+    fields = ["title", "description", "complete"]
     success_url = reverse_lazy('todoPage')
 
-class DeleteTask(DeleteView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.save()
+        return super().form_valid(form)
+    
+class UpdateTask(LoginRequiredMixin, UpdateView):
+    template_name = "todo/updateTaskDetails.html"
+    model = models.Task
+    fields = ["title", "description", "complete"]
+    success_url = reverse_lazy('todoPage')
+
+class DeleteTask(LoginRequiredMixin, DeleteView):
     template_name = "todo/deleteTask.html"
     model = models.Task
     context_object_name = "task"
