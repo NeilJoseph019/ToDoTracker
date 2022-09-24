d998102e745137de6b37922f222664faffde0a6d
Neil Joseph Pulikkathara <neil.joseph@ibm.com>
2022-09-24 14:15:26 +0100

    Login view created, need to work on register page moving forward.

--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.contrib.auth.views import LoginView, LogoutView
+
+from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from . import models
